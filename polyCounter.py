15907dd49e01ef1e73c337e987256ee1e48838b7
johnwarejunior <john@johnwarejunior.com>
2023-10-06 15:46:23 -0700

    Build polygon counting function for OBJs.

--- polyCounter.py
+++ polyCounter.py
@@ -4,3 +4,8 @@ from tkinter import filedialog
 from tkinter import messagebox
 from pywavefront import Wavefront
 
+# Function to calculate polygon count of an OBJ file
+def calculate_obj_polygon_count(obj_filename):
+    obj = Wavefront(obj_filename)
+    polygon_count = len(obj.mesh_list[0].faces)
+    return polygon_count
