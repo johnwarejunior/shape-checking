d583a6876c0febe4feb4cc0ea4ffcddb11e9d89d
johnwarejunior <john@johnwarejunior.com>
2023-10-06 20:53:37 -0700

    Handling errors. Still has issues.

--- polyCounter.py
+++ polyCounter.py
@@ -12,23 +12,26 @@ def calculate_obj_polygon_count(obj_filename):
 
 # Function processing OBJ files and checking polygon counts
 def process_files():
-    folder_path = filedialog.askdirectory(title="Select a folder containing OBJ files") 
+    folder_path = filedialog.askdirectory(title="Select a folder containing OBJ files")
 
     if not folder_path:
         return
-    
-    polygon_threshold = int(entry_threshold.get())
 
+    polygon_threshold = int(entry_threshold.get())
+    
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
-
+        
         if filename.lower().endswith('.obj'):
-            polygon_count = calculate_obj_polygon_count(file_path)
+            try:
+                polygon_count = calculate_obj_polygon_count(file_path)
+                if polygon_count >= polygon_threshold:
+                    result_label.config(text=f"{filename}: Polygon count exceeds {polygon_threshold}")
+                else:
+                    result_label.config(text=f"{filename}: Polygon count below {polygon_threshold}")
+            except Exception as e:
+                print(f"Error processing {filename}: {str(e)}")
 
-            if polygon_count >= polygon_threshold:
-                result_label.config(text=f"{filename}: Polygon count exceeds {polygon_threshold}")
-            else:
-                result_label.config(text=f"{filename}: Polygon count below {polygon_threshold}")
 
 # Create main window
 root = tk.Tk()
